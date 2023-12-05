f9191e63041a8bc5c9866ea0fe0d853a85e3d7e0
ryugibo <7928632+ryugibo@users.noreply.github.com>
2023-12-06 01:29:31 +0900

    default routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,36 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Layout from "./components/layout";
+import Home from "./routes/home";
+import Login from "./routes/login";
+import CreateAccount from "./routes/create-account";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "create-account",
+    element: <CreateAccount />,
+  },
+]);
+
 function App() {
-  return <>www</>;
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
 export default App;
