bbff879d01c4399ab3546556a3ea1fb4fa06794a
ryugibo <7928632+ryugibo@users.noreply.github.com>
2023-12-06 01:59:57 +0900

    깃허브 로그인, 로그아웃

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,9 +1,19 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import { auth } from "../firebase";
+
+const Button = styled.button``;
 
 export default function Layout() {
+  const navigate = useNavigate();
+  const onClickLogout = async () => {
+    await auth.signOut();
+    navigate("/login");
+  };
   return (
     <>
       <h2>layout</h2>
+      <Button onClick={onClickLogout}>Log out</Button>
       <Outlet />
     </>
   );
--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -1,3 +1,10 @@
+import GithubButton from "../components/github-button";
+
 export default function Login() {
-  return <h1>login</h1>;
+  return (
+    <>
+      <h1>login</h1>
+      <GithubButton />
+    </>
+  );
 }
